d0029c135938dd6e9a6b7bb66bc8a8ecd8f06456
jsvander <Jacob Vander Plaats>
2019-11-20 14:46:53 -0500

    1.0

---
+++ Program3/prog3data.js
@@ -0,0 +1,1031 @@
+var people = [
+ {
+   "firstname": "Hayden",
+   "lastname": "Alig",
+   "city": "Gibsonia",
+   "state": "PA",
+   "zipcode": 15044
+ },
+ {
+   "firstname": "Cheyanne",
+   "lastname": "Allen",
+   "city": "Ellwood City",
+   "state": "PA",
+   "zipcode": 16117
+ },
+ {
+   "firstname": "Pam",
+   "lastname": "Allen",
+   "city": "Ellwood City",
+   "state": "PA",
+   "zipcode": 16117
+ },
+ {
+   "firstname": "Sadie",
+   "lastname": "Allen",
+   "city": "Ellwood City",
+   "state": "PA",
+   "zipcode": 16117
+ },
+ {
+   "firstname": "TJ",
+   "lastname": "Allen",
+   "city": "Ellwood City",
+   "state": "PA",
+   "zipcode": 16117
+ },
+ {
+   "firstname": "Xavier",
+   "lastname": "Allen",
+   "city": "Ellwood City",
+   "state": "PA",
+   "zipcode": 16117
+ },
+ {
+   "firstname": "Annalisa",
+   "lastname": "Anglum",
+   "city": "Zelienople",
+   "state": "PA",
+   "zipcode": 16063
+ },
+ {
+   "firstname": "Marty",
+   "lastname": "Anglum",
+   "city": "Zelienople",
+   "state": "PA",
+   "zipcode": 16063
+ },
+ {
+   "firstname": "Taylor",
+   "lastname": "Anglum",
+   "city": "Zelienople",
+   "state": "PA",
+   "zipcode": 16063
+ },
+ {
+   "firstname": "Luke",
+   "lastname": "Augustine",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Donata",
+   "lastname": "Babinsack",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Marcel",
+   "lastname": "Barzen",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Lennon",
+   "lastname": "Benjamin",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Matthew",
+   "lastname": "Benjamin",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Max",
+   "lastname": "Benjamin",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Aiden",
+   "lastname": "Benton",
+   "city": "Slippery Rock",
+   "state": "PA",
+   "zipcode": 16057
+ },
+ {
+   "firstname": "Hannah",
+   "lastname": "Benton",
+   "city": "New Castle",
+   "state": "PA",
+   "zipcode": 16101
+ },
+ {
+   "firstname": "Iysis",
+   "lastname": "Benton",
+   "city": "New Castle",
+   "state": "PA",
+   "zipcode": 16101
+ },
+ {
+   "firstname": "Paxton",
+   "lastname": "Benton",
+   "city": "Slippery Rock",
+   "state": "PA",
+   "zipcode": 16057
+ },
+ {
+   "firstname": "Kim",
+   "lastname": "Berkebile",
+   "city": "Seven Fields",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Sarah",
+   "lastname": "Bestwick",
+   "city": "Renfrew",
+   "state": "PA",
+   "zipcode": 16053
+ },
+ {
+   "firstname": "Tauryn",
+   "lastname": "Black",
+   "city": "Chicora",
+   "state": "PA",
+   "zipcode": 16025
+ },
+ {
+   "firstname": "Addison",
+   "lastname": "Boer",
+   "city": "Jerome",
+   "state": "ID",
+   "zipcode": 83338
+ },
+ {
+   "firstname": "Henry",
+   "lastname": "Boots",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Broderick",
+   "lastname": "Bordt",
+   "city": "Cranberry",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Easton",
+   "lastname": "Bowser",
+   "city": "Beaver Falls",
+   "state": "PA",
+   "zipcode": 15010
+ },
+ {
+   "firstname": "Harmony",
+   "lastname": "Bowser",
+   "city": "Beaver Falls",
+   "state": "PA",
+   "zipcode": 15010
+ },
+ {
+   "firstname": "Mark",
+   "lastname": "Bowser",
+   "city": "Beaver Falls",
+   "state": "PA",
+   "zipcode": 15010
+ },
+ {
+   "firstname": "Mikayla",
+   "lastname": "Bowser",
+   "city": "Tarentum",
+   "state": "PA",
+   "zipcode": 15084
+ },
+ {
+   "firstname": "Ryder",
+   "lastname": "Bowser",
+   "city": "Beaver Falls",
+   "state": "PA",
+   "zipcode": 15010
+ },
+ {
+   "firstname": "Brooklyn",
+   "lastname": "Brady",
+   "city": "Moon Township",
+   "state": "PA",
+   "zipcode": 15108
+ },
+ {
+   "firstname": "Vivian",
+   "lastname": "Brady",
+   "city": "Mars",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Silas",
+   "lastname": "Brandon",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Gabby",
+   "lastname": "Brandt",
+   "city": "Zelienople",
+   "state": "PA",
+   "zipcode": 16063
+ },
+ {
+   "firstname": "John",
+   "lastname": "Bream",
+   "city": "Enola",
+   "state": "PA",
+   "zipcode": 17025
+ },
+ {
+   "firstname": "Adam",
+   "lastname": "Brenize",
+   "city": "Pittsburgh",
+   "state": "PA",
+   "zipcode": 15205
+ },
+ {
+   "firstname": "Beorn",
+   "lastname": "Briere",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Helen",
+   "lastname": "Brink",
+   "city": "Evans City",
+   "state": "PA",
+   "zipcode": 16033
+ },
+ {
+   "firstname": "Kiyan",
+   "lastname": "Brink",
+   "city": "Evans City",
+   "state": "PA",
+   "zipcode": 16033
+ },
+ {
+   "firstname": "Taylor",
+   "lastname": "Burd",
+   "city": "Harmony",
+   "state": "PA",
+   "zipcode": 16037
+ },
+ {
+   "firstname": "Tristan",
+   "lastname": "Burd",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Maya",
+   "lastname": "Burns",
+   "city": "Sandy Lake",
+   "state": "PA",
+   "zipcode": 16145
+ },
+ {
+   "firstname": "Gabriel",
+   "lastname": "Cale",
+   "city": "Cranberry Twp",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Sam",
+   "lastname": "Cale",
+   "city": "Cranberry Twp",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Madison",
+   "lastname": "Cammisa",
+   "city": "Renfrew",
+   "state": "PA",
+   "zipcode": 16053
+ },
+ {
+   "firstname": "Elias",
+   "lastname": "Campbell",
+   "city": "Zelienople",
+   "state": "PA",
+   "zipcode": 16063
+ },
+ {
+   "firstname": "Lily",
+   "lastname": "Campbell",
+   "city": "Zelienople",
+   "state": "PA",
+   "zipcode": 16063
+ },
+ {
+   "firstname": "Kristina",
+   "lastname": "Cannon",
+   "city": "Ellwood City",
+   "state": "PA",
+   "zipcode": 16117
+ },
+ {
+   "firstname": "Don",
+   "lastname": "Cardinal",
+   "city": "Zelienople",
+   "state": "PA",
+   "zipcode": 16063
+ },
+ {
+   "firstname": "Jaydon",
+   "lastname": "Carney",
+   "city": "Centreville",
+   "state": "VA",
+   "zipcode": 20121
+ },
+ {
+   "firstname": "Lyndsey",
+   "lastname": "Carnuche",
+   "city": "New Castle",
+   "state": "PA",
+   "zipcode": 16102
+ },
+ {
+   "firstname": "Susan",
+   "lastname": "Carroll",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Sharon",
+   "lastname": "Chrin",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Stephanie",
+   "lastname": "Christian",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Sydney",
+   "lastname": "Clark",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Grace",
+   "lastname": "Clary",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Max",
+   "lastname": "Coachman",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16002
+ },
+ {
+   "firstname": "Dallas",
+   "lastname": "Cole",
+   "city": "Cranberry",
+   "state": "Pa",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Cael",
+   "lastname": "Colwell",
+   "city": "Pittsburgh",
+   "state": "PA",
+   "zipcode": 15205
+ },
+ {
+   "firstname": "Emily",
+   "lastname": "Colwell",
+   "city": "Pittsburgh",
+   "state": "PA",
+   "zipcode": 15205
+ },
+ {
+   "firstname": "Chris",
+   "lastname": "Comack",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Claire",
+   "lastname": "Coyne",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Abram",
+   "lastname": "Crawford",
+   "city": "Mars",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Dan",
+   "lastname": "Crawford",
+   "city": "Mars",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Zayden",
+   "lastname": "Crawford",
+   "city": "Clymer",
+   "state": "PA",
+   "zipcode": 15728
+ },
+ {
+   "firstname": "Addyson",
+   "lastname": "Cujas",
+   "city": "Freedom",
+   "state": "PA",
+   "zipcode": 15042
+ },
+ {
+   "firstname": "Brandon",
+   "lastname": "Cujas",
+   "city": "Freedom",
+   "state": "PA",
+   "zipcode": 15042
+ },
+ {
+   "firstname": "Andrew",
+   "lastname": "Cypher",
+   "city": "Bradfordwoods",
+   "state": "PA",
+   "zipcode": 15015
+ },
+ {
+   "firstname": "Aaron",
+   "lastname": "Davis",
+   "city": "Tellico Plains",
+   "state": "TN",
+   "zipcode": 37385
+ },
+ {
+   "firstname": "Connor",
+   "lastname": "Denne",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Eddie",
+   "lastname": "Denne",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Tyler",
+   "lastname": "Dietrich",
+   "city": "Pittsburgh",
+   "state": "PA",
+   "zipcode": 15237
+ },
+ {
+   "firstname": "Landen",
+   "lastname": "Dimond",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Carolyn",
+   "lastname": "Dinardo",
+   "city": "New Brighton",
+   "state": "PA",
+   "zipcode": 15066
+ },
+ {
+   "firstname": "Faith",
+   "lastname": "Dinardo",
+   "city": "New Brighton",
+   "state": "PA",
+   "zipcode": 15066
+ },
+ {
+   "firstname": "Felix",
+   "lastname": "Dinardo",
+   "city": "New Brighton",
+   "state": "PA",
+   "zipcode": 15066
+ },
+ {
+   "firstname": "Hope",
+   "lastname": "Dougherty",
+   "city": "Gibsonia",
+   "state": "PA",
+   "zipcode": 15044
+ },
+ {
+   "firstname": "James",
+   "lastname": "Douglass",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Casey",
+   "lastname": "Dully",
+   "city": "Bethany",
+   "state": "PA",
+   "zipcode": 26032
+ },
+ {
+   "firstname": "Roxanne",
+   "lastname": "Dully",
+   "city": "Bethany",
+   "state": "PA",
+   "zipcode": 26032
+ },
+ {
+   "firstname": "Taylor",
+   "lastname": "Dully",
+   "city": "Bethany",
+   "state": "PA",
+   "zipcode": 26032
+ },
+ {
+   "firstname": "Thomas",
+   "lastname": "Dully",
+   "city": "Bethany",
+   "state": "PA",
+   "zipcode": 26032
+ },
+ {
+   "firstname": "Tiffani",
+   "lastname": "Dully",
+   "city": "Bethany",
+   "state": "PA",
+   "zipcode": 26032
+ },
+ {
+   "firstname": "Isabella",
+   "lastname": "Dunn",
+   "city": "Dover",
+   "state": "PA",
+   "zipcode": 44622
+ },
+ {
+   "firstname": "Aaron",
+   "lastname": "Eberlin",
+   "city": "Grove City",
+   "state": "PA",
+   "zipcode": 16127
+ },
+ {
+   "firstname": "Carson",
+   "lastname": "Elder",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16002
+ },
+ {
+   "firstname": "Isabella",
+   "lastname": "Elder",
+   "city": "Fombell",
+   "state": "PA",
+   "zipcode": 16123
+ },
+ {
+   "firstname": "Kathryn",
+   "lastname": "Ellsworth",
+   "city": "New Brighton",
+   "state": "PA",
+   "zipcode": 15066
+ },
+ {
+   "firstname": "Connor",
+   "lastname": "Ewing",
+   "city": "Freedom",
+   "state": "PA",
+   "zipcode": 15042
+ },
+ {
+   "firstname": "Maddie",
+   "lastname": "Fairweather",
+   "city": "Ellwood City",
+   "state": "PA",
+   "zipcode": 16117
+ },
+ {
+   "firstname": "William",
+   "lastname": "Feliciano",
+   "city": "Orange",
+   "state": "TX",
+   "zipcode": 77630
+ },
+ {
+   "firstname": "Concetta",
+   "lastname": "Fields",
+   "city": "New Brighton",
+   "state": "PA",
+   "zipcode": 15066
+ },
+ {
+   "firstname": "Mary",
+   "lastname": "Francona",
+   "city": "Monaca",
+   "state": "PA",
+   "zipcode": 15061
+ },
+ {
+   "firstname": "Melissa",
+   "lastname": "Frazier",
+   "city": "Prospect",
+   "state": "PA",
+   "zipcode": 16052
+ },
+ {
+   "firstname": "Gabriel",
+   "lastname": "Fuchs",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Rowena",
+   "lastname": "Fuchs",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Stephanie",
+   "lastname": "Fuchs",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Troy",
+   "lastname": "Fuchs",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Ashlyn",
+   "lastname": "Gamble",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Andromeda",
+   "lastname": "Ghelarducci",
+   "city": "Bridgeville",
+   "state": "PA",
+   "zipcode": 15017
+ },
+ {
+   "firstname": "Selah",
+   "lastname": "Gilg",
+   "city": "Zelienople",
+   "state": "PA",
+   "zipcode": 16063
+ },
+ {
+   "firstname": "Maximus",
+   "lastname": "Gillett",
+   "city": "Cranberry Twp",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Dylan",
+   "lastname": "Gira",
+   "city": "Monroeville",
+   "state": "PA",
+   "zipcode": 15146
+ },
+ {
+   "firstname": "Ayirson",
+   "lastname": "Gitts",
+   "city": "Beaver Falls",
+   "state": "PA",
+   "zipcode": 15010
+ },
+ {
+   "firstname": "Blake",
+   "lastname": "Gonzalez",
+   "city": "Seven Fields",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Luca",
+   "lastname": "Gonzalez",
+   "city": "Seven Fields",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Megan",
+   "lastname": "Gonzalez",
+   "city": "Seven Fields",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Travis",
+   "lastname": "Gonzalez",
+   "city": "Seven Fields",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Trent",
+   "lastname": "Gonzalez",
+   "city": "Seven Fields",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Jordan",
+   "lastname": "Goodlin",
+   "city": "Monaca",
+   "state": "PA",
+   "zipcode": 15061
+ },
+ {
+   "firstname": "Camryn",
+   "lastname": "Goucher",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Susan",
+   "lastname": "Goucher",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Dan",
+   "lastname": "Graf",
+   "city": "Sandy",
+   "state": "UT",
+   "zipcode": 84094
+ },
+ {
+   "firstname": "Gabrielle",
+   "lastname": "Green",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Trevor",
+   "lastname": "Green",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Maysen",
+   "lastname": "Grim",
+   "city": "Zelienople",
+   "state": "PA",
+   "zipcode": 16063
+ },
+ {
+   "firstname": "Evolette",
+   "lastname": "Hair",
+   "city": "Pittsburgh",
+   "state": "PA",
+   "zipcode": 15205
+ },
+ {
+   "firstname": "Joycelynn",
+   "lastname": "Hardie",
+   "city": "Freedom",
+   "state": "PA",
+   "zipcode": 15042
+ },
+ {
+   "firstname": "Braden",
+   "lastname": "Harley",
+   "city": "Mars",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Isaac",
+   "lastname": "Harris",
+   "city": "Wampum",
+   "state": "PA",
+   "zipcode": 16157
+ },
+ {
+   "firstname": "Maverick",
+   "lastname": "Harris",
+   "city": "Wampum",
+   "state": "PA",
+   "zipcode": 16157
+ },
+ {
+   "firstname": "Rachel",
+   "lastname": "Hartman",
+   "city": "Mars",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Joey",
+   "lastname": "Heeter",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Caity",
+   "lastname": "Henderson",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Henry",
+   "lastname": "Henne",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Emma",
+   "lastname": "Henry",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Madison",
+   "lastname": "Hernandez",
+   "city": "Monroeville",
+   "state": "PA",
+   "zipcode": 15146
+ },
+ {
+   "firstname": "Brielle",
+   "lastname": "Hibbard",
+   "city": "BUTLER",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Barbara",
+   "lastname": "Hickly",
+   "city": "Grove City",
+   "state": "PA",
+   "zipcode": 16127
+ },
+ {
+   "firstname": "Zoe",
+   "lastname": "Hickly",
+   "city": "Grove City",
+   "state": "PA",
+   "zipcode": 16127
+ },
+ {
+   "firstname": "Emma",
+   "lastname": "Holmes",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Eric",
+   "lastname": "Holmes",
+   "city": "Roddick",
+   "state": "FL",
+   "zipcode": 32686
+ },
+ {
+   "firstname": "Gary",
+   "lastname": "Holmes",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Kellan",
+   "lastname": "Holmes",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Kim",
+   "lastname": "Holsinger",
+   "city": "Beaver Falls",
+   "state": "PA",
+   "zipcode": 15010
+ },
+ {
+   "firstname": "Bella",
+   "lastname": "Holt",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Nicky",
+   "lastname": "Holt",
+   "city": "Butler",
+   "state": "PA",
+   "zipcode": 16001
+ },
+ {
+   "firstname": "Chelsie",
+   "lastname": "Homick",
+   "city": "Baden",
+   "state": "PA",
+   "zipcode": 15005
+ },
+ {
+   "firstname": "Oliver",
+   "lastname": "Homick",
+   "city": "Baden",
+   "state": "PA",
+   "zipcode": 15005
+ },
+ {
+   "firstname": "Tamara",
+   "lastname": "Homick",
+   "city": "Baden",
+   "state": "PA",
+   "zipcode": 15005
+ },
+ {
+   "firstname": "Iliana",
+   "lastname": "Horgos",
+   "city": "Rochester",
+   "state": "PA",
+   "zipcode": 15074
+ },
+ {
+   "firstname": "Samantha",
+   "lastname": "Hubler",
+   "city": "Cranberry Township",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Jax",
+   "lastname": "Huff",
+   "city": "Mars",
+   "state": "PA",
+   "zipcode": 16046
+ },
+ {
+   "firstname": "Anna",
+   "lastname": "Huffman",
+   "city": "Cranberry Twp",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Dick",
+   "lastname": "Huffman",
+   "city": "Cranberry Twp",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Joseph",
+   "lastname": "Huffman",
+   "city": "Cranberry Twp",
+   "state": "PA",
+   "zipcode": 16066
+ },
+ {
+   "firstname": "Joy",
+   "lastname": "Huffman",
+   "city": "Cranberry Twp",
+   "state": "PA",
+   "zipcode": 16066
+ }
+];
